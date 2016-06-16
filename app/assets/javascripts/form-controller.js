003d06f3819e9aaee768019e97a98e60c57deddf
Herschel <herschel.kulkarni@gmail.com>
2016-06-15 22:25:25 -0500

    Add dummy form object and ng-repeat.

---
+++ app/assets/javascripts/form-controller.js
@@ -0,0 +1,89 @@
+(function() {
+    'use strict';
+    angular.module('careAnalytics')
+      .controller('FormController', FormController);
+
+    FormController.$inject = ['$http'];
+
+    function FormController($http) {
+      var self = this;
+      self.email;
+      self.template = [
+        {
+          label: "Patient's Name",
+          type: "container",
+          children: [
+            {
+              label: "Last",
+              value: "",
+              type: "leaf"
+            },
+            {
+              label: "First",
+              value: "",
+              type: "leaf"
+            },
+            {
+              label: "Middle",
+              value: "",
+              type: "leaf"
+            }
+          ],
+          code: "PATIENT_NAME",
+          value: ""
+        },
+        {
+          label: "Mailing Address",
+          type: "container",
+          children: [
+            {
+              label: "Street",
+              value: "",
+              type: "leaf"
+            },
+            {
+              label: "City",
+              value: "",
+              type: "leaf"
+            },
+            {
+              label: "Zip",
+              value: "",
+              type: "leaf"
+            }
+          ],
+          code: "MAILING_ADDRESS",
+          value: ""
+        },
+        {
+          label: "",
+          type: "parent",
+          children: [
+            {
+              label: "Home Phone",
+              value: "",
+              type: "leaf"
+            },
+            {
+              label: "Work Phone",
+              value: "",
+              type: "leaf"
+            },
+            {
+              label: "Cell Phone",
+              value: "",
+              type: "leaf"
+            }
+          ],
+          code: "Phone",
+          value: ""
+        }
+      ];
+
+      console.log(self.template);
+
+      self.showInputForm = function() {
+        return !self.buttonClicked;
+      };
+    }
+})();
