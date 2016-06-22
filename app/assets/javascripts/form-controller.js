e5a38735e0725f84cea13662d8e0ab0fad8d0d6d
Herschel <herschel.kulkarni@gmail.com>
2016-06-21 20:32:20 -0500

    Add signature library - Initial commit.

--- app/assets/javascripts/form-controller.js
+++ app/assets/javascripts/form-controller.js
@@ -7,7 +7,6 @@
 
     function FormController($http) {
       var self = this;
-      self.email;
       self.template = [
         {
           label: "Patient's Name",
@@ -82,8 +81,48 @@
 
       console.log(self.template);
 
-      self.showInputForm = function() {
-        return !self.buttonClicked;
-      };
+      // ----------------------- Signature code ----------------------- //
+
+      var wrapper = document.getElementById("signature-pad"),
+      clearButton = wrapper.querySelector("[data-action=clear]"),
+      saveButton = wrapper.querySelector("[data-action=save]"),
+      canvas = wrapper.querySelector("canvas"),
+      signaturePad;
+
+      // Adjust canvas coordinate space taking into account pixel ratio,
+      // to make it look crisp on mobile devices.
+      // This also causes canvas to be cleared.
+      function resizeCanvas() {
+          // When zoomed out to less than 100%, for some very strange reason,
+          // some browsers report devicePixelRatio as less than 1
+          // and only part of the canvas is cleared then.
+          var ratio =  Math.max(window.devicePixelRatio || 1, 1);
+          canvas.width = canvas.offsetWidth * ratio;
+          canvas.height = canvas.offsetHeight * ratio;
+          canvas.getContext("2d").scale(ratio, ratio);
+      }
+
+      window.onresize = resizeCanvas;
+      resizeCanvas();
+
+      signaturePad = new SignaturePad(canvas);
+
+      clearButton.addEventListener("click", function (event) {
+          signaturePad.clear();
+      });
+
+      saveButton.addEventListener("click", function (event) {
+          if (signaturePad.isEmpty()) {
+              alert("Please provide signature first.");
+          } else {
+              window.open(signaturePad.toDataURL());
+          }
+      });
+
+      // ----------------------- End Signature code ----------------------- //
+
+
     }
+
+
 })();
