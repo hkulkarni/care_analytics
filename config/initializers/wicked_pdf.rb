c5ff849cfaea26f88a67540617fed233458dcde5
Herschel <herschel.kulkarni@gmail.com>
2016-07-25 20:36:56 -0500

    Remove exe path for wicked pdf.

--- config/initializers/wicked_pdf.rb
+++ config/initializers/wicked_pdf.rb
@@ -8,16 +8,4 @@
 #
 # https://github.com/mileszs/wicked_pdf/blob/master/README.md
 
-# WickedPdf.config = {
-#   exe_path: '/usr/local/bin/wkhtmltopdf'
-# }
-
-platform = RUBY_PLATFORM 
-
-if platform.include?("darwin") # OS X machine 
-  binary_path = Rails.root.join('bin', 'wkhtmltopdf-0.9.9-OS-X-i386').to_s 
-elsif platform.include?("64-linux") # 64-bit linux machine 
-  binary_path = Rails.root.join('bin', 'wkhtmltopdf-amd64').to_s 
-end 
-
-WickedPdf.config = { :exe_path => '/Users/herschel/.rvm/gems/ruby-2.2.1/bin/wkhtmltopdf' }
+WickedPdf.config = {}
