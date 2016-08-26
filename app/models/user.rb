8381411ce1702416455cd583a241d32139831e20
Herschel <herschel.kulkarni@gmail.com>
2016-08-25 21:47:29 -0500

    Downcase file name for template.

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ActiveRecord::Base
     has_secure_password
 
     def file_name
-        "#{first_name}_#{last_name}"
+        "#{first_name.downcase}_#{last_name.downcase}"
     end
     
 end
