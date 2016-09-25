8ee467c9ce9a82a995697d8b7fb7157b91811750
Herschel <herschel.kulkarni@gmail.com>
2016-09-24 23:41:18 -0500

    Remove logout link and rescue invalid user exception.

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,11 @@ class User < ActiveRecord::Base
     has_secure_password
 
     def file_name
-        "#{first_name.downcase}_#{last_name.downcase}"
+        begin
+            "#{first_name.downcase}_#{last_name.downcase}"
+        rescue
+            Rails.logger.info("Attempted to access invalid user")
+        end
     end
     
 end
