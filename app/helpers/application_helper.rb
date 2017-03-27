525c82e7a06ccf9a48f84d1edc48f221b4ccb22c
Herschel <herschel.kulkarni@gmail.com>
2017-03-27 10:50:45 -0500

    Add new formudent marketing form.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -14,6 +14,8 @@ module ApplicationHelper
             return "daniel_gehani.png"
         when "hardik_chodavadia"
             return "hardik_chodavadia.png"
+        when "formudent_services"
+            return "formudent.png"
         else
             return ''
         end
@@ -25,7 +27,8 @@ module ApplicationHelper
         current_user.file_name == 'herschel_kulkarni' ||
         current_user.file_name == 'samir_kulkarni' ||
         current_user.file_name == 'daniel_gehani' ||
-        current_user.file_name == 'hardik_chodavadia'
+        current_user.file_name == 'hardik_chodavadia' ||
+        current_user.file_name == 'formudent_services'
     end
 
 end
